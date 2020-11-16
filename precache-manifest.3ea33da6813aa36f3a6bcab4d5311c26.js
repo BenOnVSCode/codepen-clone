616ddf043bd997240bfd2dbc019b79180d4c4ae9
4bdeladim <abdeladim.frontend@gmail.com>
2020-11-16 17:33:15 +0100

    Updates

---
+++ precache-manifest.3ea33da6813aa36f3a6bcab4d5311c26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2411de538e7a8d1530ecfd50fb4fada",
+    "url": "/codepen-clone%20/index.html"
+  },
+  {
+    "revision": "514edd953b47a3a1a886",
+    "url": "/codepen-clone%20/static/css/2.58ec3025.chunk.css"
+  },
+  {
+    "revision": "3d5784a20e1092531a91",
+    "url": "/codepen-clone%20/static/css/main.392f2671.chunk.css"
+  },
+  {
+    "revision": "514edd953b47a3a1a886",
+    "url": "/codepen-clone%20/static/js/2.1aa0f0be.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/codepen-clone%20/static/js/2.1aa0f0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5784a20e1092531a91",
+    "url": "/codepen-clone%20/static/js/main.2ae444fa.chunk.js"
+  },
+  {
+    "revision": "2382b6756d9a5872ede4",
+    "url": "/codepen-clone%20/static/js/runtime-main.ff9895d6.js"
+  }
+]);
